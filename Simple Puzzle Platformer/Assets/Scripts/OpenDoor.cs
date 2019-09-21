1bf1b63443eb38df2c2bbf9e434e333b0788ed3a
JakeHresh <jakeyhresh123@gmail.com>
2019-09-21 10:46:57 -0700

    Added Alana's scripts into project

---
+++ Simple Puzzle Platformer/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    Animation hingehere; // you're gonna need a collider in the map for this thing to  interact with
+    //AudioSource locked;
+    //AudioSource unlocked; for sound fx 
+
+    void OnTriggerStay()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            hingehere.Play();
+            //unlocked.Play();
+        }
+    }
+}
---
+++ Simple Puzzle Platformer/Assets/Scripts/PickKey.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickKey : MonoBehaviour
+{
+    public Component doorcollider;
+    public GameObject key;
+    void OnTriggerStay()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            doorcollider.GetComponent<BoxCollider>().enabled = true;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            key.SetActive(false);
+        }
+    }
+}
