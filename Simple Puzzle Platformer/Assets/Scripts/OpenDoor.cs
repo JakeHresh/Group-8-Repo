c01c0293092b143e5885c92ff0bee7dc20bc03c7
JakeHresh <jakeyhresh123@gmail.com>
2019-09-22 20:18:33 -0700

    Added Victory Scene

--- Simple Puzzle Platformer/Assets/Scripts/OpenDoor.cs
+++ Simple Puzzle Platformer/Assets/Scripts/OpenDoor.cs
@@ -7,8 +7,10 @@ public class OpenDoor : MonoBehaviour
 {
     public GameObject player;
     Animation hingehere; // you're gonna need a collider in the map for this thing to  interact with
-    public AudioClip GateClip;
-    public AudioSource GateSource;
+    public AudioClip GateClip1;
+    public AudioSource GateSource1;
+    public AudioClip GateClip2;
+    public AudioSource GateSource2;
     //AudioSource locked;
     //AudioSource unlocked; for sound fx 
 
@@ -16,19 +18,24 @@ public class OpenDoor : MonoBehaviour
     {
         if (Col.gameObject.tag == "Door")
         {
-            if (Input.GetKey("e") && player.GetComponent<KeyBehavior>().keyCount > 0)
+            if (Input.GetKeyDown("e") && player.GetComponent<KeyBehavior>().keyCount > 0)
             {
                 Col.GetComponent<MoveDoor>().canOpen = true;
                 player.GetComponent<KeyBehavior>().keyCount--;
-                GateSource.Play();
+                GateSource1.Play();
                 //hingehere.Play();
                 //unlocked.Play();
+            }
+            else if(Input.GetKeyDown("e"))
+            {
+                GateSource2.Play();
             }
         }
     }
 
     private void Start()
     {
-        GateSource.clip = GateClip;
+        GateSource1.clip = GateClip1;
+        GateSource2.clip = GateClip2;
     }
 }
